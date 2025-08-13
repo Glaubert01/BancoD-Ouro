e74c08f18790be802c39cf278b0f4b28d2ec3db1
Glaubert01 <glaubert@outlook.com>
2025-08-12 22:43:53 -0300

    feat: Finalizada a tela de login

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -6,12 +6,58 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Text("Sistema de Gestão de Contas."),
-          TextFormField(),
-          TextFormField(),
-          ElevatedButton(onPressed: () {}, child: Text("Entrar")),
+      body: Stack(
+        children: [
+          Image.asset("assets/banner.png"),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset("assets/stars.png"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 80),
+                Image.asset("assets/logo.png", width: 120),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 32),
+                    const Text(
+                      "Sistema de Gestão de Contas",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 32),
+                    ),
+                    const SizedBox(height: 32),
+                    TextFormField(
+                      decoration: const InputDecoration(label: Text("Email")),
+                    ),
+                    const SizedBox(height: 32),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(label: Text("Senha")),
+                    ),
+                    const SizedBox(height: 32),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, "home");
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                          Color(0xFFFFA902),
+                        ),
+                      ),
+                      child: Text(
+                        "Entrar",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
